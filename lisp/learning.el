aabcdc44d1f8ca53cac0258e71195046dab5370a
Bryan P Johnson <bryan@12ftguru.com>
2015-12-08 14:27:17 -0500

    Add cheatsheet items.
    Signed-off-by: Bryan P Johnson <bryan@12ftguru.com>

--- lisp/learning.el
+++ lisp/learning.el
@@ -1,18 +1,60 @@
 (use-package which-key
   :init (which-key-mode)
   :config (progn
-	    (which-key-setup-minibuffer)
-	    )
+            (which-key-setup-minibuffer)
+            )
   )
 
 (use-package cheatsheet
   :bind ("<f12>" . cheatsheet-show)
-  :config (progn
-	    ;;Add Cheatsheet entries here.
-	    (cheatsheet-add :group 'Notes
-                :key "C-c c p"
-                :description "Add Project Todo")
-	    )
+  :config
+  (progn
+    ;;Add Cheatsheet entries here.
+    (cheatsheet-add
+     :group 'Projects
+     :key "C-c p p"
+     :description "Create New Project")
+    (cheatsheet-add
+     :group 'Projects
+     :key "M-o"
+     :description "Open Project")
+    (cheatsheet-add
+     :group 'Projects
+     :key "M-p"
+     :description "Switch Project Window")
+    (cheatsheet-add
+     :group 'Projects
+     :key "C-c c p"
+     :description "Add Project Todo")
+    (cheatsheet-add
+     :group 'Multi-Cursor
+     :key "C->"
+     :description "Mark Next")
+    (cheatsheet-add
+     :group 'Multi-Cursor
+     :key "C-<"
+     :description "Mark Prev")
+    (cheatsheet-add
+     :group 'Multi-Cursor
+     :key "C-c C-<"
+     :description "Mark All")
+    (cheatsheet-add
+     :group 'Parens
+     :key "M-<up>"
+     :description "Move Up Sexp")
+    (cheatsheet-add
+     :group 'Parens
+     :key "M-<down>"
+     :description "Move Down Sexp")
+    (cheatsheet-add
+     :group 'Parens
+     :key "C-c ("
+     :description "Wrap Selection")
+    (cheatsheet-add
+     :group 'Parens
+     :key "C-d"
+     :description "Multiline Sexp")
+    )
   )
 
 (provide 'learning)
